cda03ed7ac5958034ec234c8156c812add67f86c
teresia <tereziah.mpaera@student>
2026-06-08 20:10:12 +0300

    feature done

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2026 Event Planner App</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <nav>
+      <h1>Event Planner App</h1>
+    </nav>
+  );
+}
+
+export default Navbar;
